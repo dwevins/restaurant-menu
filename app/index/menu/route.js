9b6b4a9f632fb2a71a4e66a1fd22d4475c665b7d
Danny Evins <woody.evins@gmail.com>
2016-11-21 12:50:59 -0600

    loading state for menu works

--- app/index/menu/route.js
+++ app/index/menu/route.js
@@ -1,5 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-
+  model() {
+    return new Ember.RSVP.Promise(function(resolve) {
+      Ember.run.later(function() {
+        resolve([
+          "top"
+        ]);
+      }, 2000);
+    });
+  }
 });
